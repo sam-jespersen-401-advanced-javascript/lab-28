800f9bc82403a06de68743b51e5d9344dfbfb702
Sam Jespersen <heyheysamj@gmail.com>
2019-10-30 17:05:16 -0700

    refactored

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,10 @@
 import React, { Component } from 'react';
-import ColorDisplay from './colors/ColorDisplay';
-import ColorPicker from './colors/ColorPicker';
+import ColorContainer from './containers/ColorContainer';
 
 export default class App extends Component {
-  state = {
-    backgroundColor: 'black'
-  };
-
-  selectColor = backgroundColor => {
-    this.setState({ backgroundColor })
-  }
-
   render() {
-    const { backgroundColor } = this.state;
-    const colors = ['red', 'green', 'black', 'yellow'];
-
-    return (
-      <>
-        <ColorDisplay backgroundColor={backgroundColor} />
-        <ColorPicker
-          selectColor={this.selectColor}
-          colors={colors} />
-      </>
-    )
+    return <>
+      <ColorContainer />
+    </>;
   }
 }
