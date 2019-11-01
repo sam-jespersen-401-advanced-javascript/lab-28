32783bf9c389ca0547318a2c65d6dc6dc979dc48
Sam Jespersen <heyheysamj@gmail.com>
2019-11-01 11:36:42 -0700

    another try at Travis

---
+++ src/components/containers/RandomColor.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import RandomColor from './RandomColor';
+
+describe('ColorContainer module', () => {
+  it('renders a color container', () => {
+    const wrapper = shallow(<RandomColor />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
