f03ce8e1c053c116abb81489c36cd0bbdc261d4a
Sam Jespersen <heyheysamj@gmail.com>
2019-10-31 17:31:16 -0700

    stupid tests

--- src/components/containers/RandomColor.js
+++ src/components/containers/RandomColor.js
@@ -12,15 +12,21 @@ export default class randomColorDisplay extends Component {
     }
 
     componentDidMount() {
+
       setInterval(() => {
+
         const newColor = randomColor();
+
         if(newColor !== this.state.backgroundColor) {
           this.setState({
-            backgroundColor: randomColor(), backgroundImage: 'none'
+            backgroundColor: randomColor(),
+            backgroundImage: 'none'
           });
+
         } else {
           this.setState({ backgroundColor: 'none', backgroundImage: 'url("https://i.dailymail.co.uk/i/pix/2016/03/18/15/324D202500000578-3498922-image-a-33_1458315465874.jpg")' });
         }
+
       }, 100);
     }
 
